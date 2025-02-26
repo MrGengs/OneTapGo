b22bc97ac42a20875c47ed6c23392ca7d694b2a1
MrGengs <sugengmargono901@gmail.com>
2025-02-26 14:15:38 +0700

    by sugeng

--- app/src/main/java/com/example/onetapgo/Settings.java
+++ app/src/main/java/com/example/onetapgo/Settings.java
@@ -15,41 +15,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class Settings extends AppCompatActivity {
-    private Switch darkModeSwitch;
-    private SharedPreferences sharedPreferences;
-    private SharedPreferences.Editor editor;
-    private ImageView btnBack;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
-
-        // Inisialisasi Toolbar
-        btnBack = findViewById(R.id.btnBack);
-
-        btnBack.setOnClickListener(v -> {
-            Intent intent = new Intent(Settings.this, MainActivity.class);
-            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
-            startActivity(intent);
-            finish();
-        });
-
-        // Inisialisasi View
-        darkModeSwitch = findViewById(R.id.darkModeSwitch);
-
-        // SharedPreferences untuk menyimpan pengaturan
-        sharedPreferences = getSharedPreferences("SettingsPrefs", MODE_PRIVATE);
-        editor = sharedPreferences.edit();
-
-        // Memuat status dark mode dari SharedPreferences
-        boolean isDarkMode = sharedPreferences.getBoolean("DARK_MODE", false);
-        darkModeSwitch.setChecked(isDarkMode);
-
-        // Simpan perubahan secara otomatis saat switch diubah
-        darkModeSwitch.setOnCheckedChangeListener((buttonView, isChecked) -> {
-            editor.putBoolean("DARK_MODE", isChecked);
-            editor.apply();
-        });
     }
 }
